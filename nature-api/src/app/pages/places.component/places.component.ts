340fb8741a9fdf80c3e38b971f74505c45cfa630
Gael Duran <ldt76578@lasallebajio.edu.mx>
2025-10-14 15:04:53 -0600

    Refactor Places and Trails components to implement proper structure and data binding

--- nature-api/src/app/pages/places.component/places.component.ts
+++ nature-api/src/app/pages/places.component/places.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlaceService } from '../../core/services/place.service';
+import { Place } from '../../core/models/place.model';
 
 @Component({
-  selector: 'app-places.component',
-  imports: [],
+  selector: 'app-places',
   templateUrl: './places.component.html',
-  styleUrl: './places.component.scss'
+  styleUrls: ['./places.component.css']
 })
-export class PlacesComponent {
+export class PlacesComponent implements OnInit {
 
+  places: Place[] = [];
+
+  constructor(private placeService: PlaceService) {}
+
+  ngOnInit(): void {
+    this.placeService.getPlaces().subscribe(data => {
+      this.places = data;
+    });
+  }
 }
--- nature-api/src/app/pages/trails.component/trails.component.ts
+++ nature-api/src/app/pages/trails.component/trails.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrailService } from '../../core/services/trail.service';
+import { Trail } from '../../core/models/trail.model';
 
 @Component({
-  selector: 'app-trails.component',
-  imports: [],
+  selector: 'app-trails',
   templateUrl: './trails.component.html',
-  styleUrl: './trails.component.scss'
+  styleUrls: ['./trails.component.css']
 })
-export class TrailsComponent {
+export class TrailsComponent implements OnInit {
 
+  trails: Trail[] = [];
+
+  constructor(private trailService: TrailService) {}
+
+  ngOnInit(): void {
+    this.trailService.getTrails().subscribe(data => {
+      this.trails = data;
+    });
+  }
 }
