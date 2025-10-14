3e73fe0e4b0ca3c470b7eeb7060bf5c98ac71ac4
Gael Duran <ldt76578@lasallebajio.edu.mx>
2025-10-14 15:05:54 -0600

    Implement place detail view with photo gallery, amenities, and mini-map integration

--- nature-api/src/app/pages/place-detail.component/place-detail.component.ts
+++ nature-api/src/app/pages/place-detail.component/place-detail.component.ts
@@ -1,21 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PlaceService } from '../../core/services/place.service';
 import { Place } from '../../core/models/place.model';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
-  selector: 'app-places',
-  templateUrl: './places.component.html',
-  styleUrls: ['./places.component.css']
+  selector: 'app-place-detail',
+  templateUrl: './place-detail.component.html',
+  styleUrls: ['./place-detail.component.css']
 })
-export class PlacesComponent implements OnInit {
+export class PlaceDetailComponent implements OnInit {
 
-  places: Place[] = [];
+  place!: Place;
+  map!: mapboxgl.Map;
 
-  constructor(private placeService: PlaceService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private placeService: PlaceService
+  ) {}
 
   ngOnInit(): void {
-    this.placeService.getPlaces().subscribe(data => {
-      this.places = data;
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.placeService.getPlaceById(id).subscribe(data => {
+      this.place = data;
+      this.loadMiniMap();
     });
+  }
+
+  loadMiniMap(): void {
+    (mapboxgl as any).accessToken = 'TU_TOKEN_DE_MAPBOX_AQUI';
+    this.map = new mapboxgl.Map({
+      container: 'mini-map',
+      style: 'mapbox://styles/mapbox/outdoors-v12',
+      center: [this.place.longitude, this.place.latitude],
+      zoom: 11
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat([this.place.longitude, this.place.latitude])
+      .setPopup(new mapboxgl.Popup().setHTML(`<b>${this.place.name}</b>`))
+      .addTo(this.map);
   }
 }
